24367567d3d98742f6afe12d135948b7621cf61c
Cohen Ariel Eitan <a.cohen@etec.local>
2023-08-17 12:57:30 -0300

    trampa aleatoria

--- Assets/Horizontal.cs
+++ Assets/Horizontal.cs
@@ -9,31 +9,36 @@ public class Horizontal : MonoBehaviour
     public float limI = 10;
     bool muevederecha = true;
 
-
+    bool activo = false;
     // Start is called before the first frame update
-    void Start()
+    public void Activar()
     {
-        
+        activo = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x > limD)
+        if (activo )
         {
-            muevederecha = false;
+            if (transform.position.x > limD)
+            {
+                muevederecha = false;
+            }
+            if (transform.position.x < limI)
+            {
+                muevederecha = true;
+            }
+            if(muevederecha == true)
+            {
+                transform.position = new Vector3(transform.position.x + avance, transform.position.y, transform.position.z);
+            }
+            if (muevederecha == false)
+            {
+                transform.position = new Vector3(transform.position.x - avance, transform.position.y, transform.position.z);
+            }
         }
-        if (transform.position.x < limI)
-        {
-            muevederecha = true;
-        }
-        if(muevederecha == true)
-        {
-            transform.position = new Vector3(transform.position.x + avance, transform.position.y, transform.position.z);
-        }
-        if (muevederecha == false)
-        {
-            transform.position = new Vector3(transform.position.x - avance, transform.position.y, transform.position.z);
-        }
+        
+
     }
 }

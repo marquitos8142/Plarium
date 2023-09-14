551567d5546e39d69c165cf565c44de71b930da5
Beltran Nicolas Marcos <n.beltran@etec.local>
2023-09-14 10:39:51 -0300

    segundointento

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -13,22 +13,42 @@ public class Character : MonoBehaviour
     public float MovementSpeed = 1;
     public float JumpForce = 1;
 
-    
+    private bool estaCorriendoAnim = false;
 
     private void Start()
     {
         _rigibody = GetComponent<Rigidbody2D>();
         _anim = GetComponent<Animator>();
         SonidodDeSalto = GetComponent<AudioSource>();
+
     }
 
     private void Update()
     {
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+        
+        if (estaCorriendoAnim== false)
+        {
+            estaCorriendoAnim = true;
+            
+            _anim.SetTrigger("correr");
+            Debug.Log("correr");
+
+        }
+        else
+        {
+            estaCorriendoAnim = false;
+            Debug.Log("no correr");
+
+
+        }
+
+        //Debug.Log(movement);
 
         if (!Mathf.Approximately(0, movement))
             transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        
 
         if (Input.GetButtonDown("Jump") )
         {
@@ -50,6 +70,7 @@ public class Character : MonoBehaviour
             _rigibody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
             _anim.SetTrigger("jump");
             SonidodDeSalto.Play();
+          
         }
     }
     void OnCollisionEnter2D(Collision2D col)
